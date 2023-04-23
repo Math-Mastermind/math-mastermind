534048149c6bc11749148bb0c03a0d3cd24ad15d
Revku <revkuu@gmail.com>
2023-04-23 16:18:09 +0200

    Stworzenie pierwszej wersji mechanizmu przejścia między stronami i podstawowa strona główna

---
+++ Pages/Home.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathMastermind.Pages
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var ClickedButton = e.OriginalSource as NavButton;
+
+            NavigationService.Navigate(ClickedButton.NavUri);
+        }
+    }
+}
